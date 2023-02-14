3de25631200706e5c600f1f1d03f2eedd562b19f
Солнце <@LAPTOP-DPKNJGCI>
2023-02-14 14:15:38 +0300

    Добавьте файлы проекта.

---
+++ СМ_12/Circle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace СМ_12
+{
+    static class Circle
+    {
+        static public double GetLine(double r)
+        {
+            return 2*Math.PI*r;
+        }
+        static public double GetSquare(double r)
+        {
+            return Math.PI*r*r;
+        }
+        static public string GetKrug (double x, double y,double r)
+        {
+            if(r== Math.Sqrt(x * x + y * y))
+            return "Точка принадлежит кругу";
+            else
+            return "Точка не принадлежит кругу";
+
+        }
+    }
+}
---
+++ СМ_12/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace СМ_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите радиус");
+            double r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите координаты х и у");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+            double square = Circle.GetSquare(r);
+            double line = Circle.GetLine(r);
+            string krug = Circle.GetKrug(x, y, r);
+            Console.WriteLine($"Длина окружности -{line:F2} \nПлощадь круга -{square:F2} \n{krug}");
+            Console.ReadKey();
+        }
+    }
+}
